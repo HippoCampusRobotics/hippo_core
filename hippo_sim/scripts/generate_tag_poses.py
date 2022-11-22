d89822a522cddf4729d147c65a4ae82905f30f0d
Thies Lennart Alff <lennart.alff@gmail.com>
2022-11-22 16:07:10 +0000

    generate apriltag_ros configuration files fitting the ros2 parameter restrictions

--- hippo_sim/scripts/generate_tag_poses.py
+++ hippo_sim/scripts/generate_tag_poses.py
@@ -37,30 +37,56 @@ def generate_even_grid(size: Tuple[int, int], offset: Tuple[float, float,
     return data
 
 
-def generate_tag_standalone(n_tags: int, size: float):
+def generate_standalone_tags(n_tags: int, size: float):
     data = {}
-    data['standalone_tags'] = []
+    data['standalone_tags'] = {}
+    data['standalone_tags']['tag_names'] = []
     for i in range(n_tags):
-        data['standalone_tags'].append({'id': i, 'size': size})
+        tag_name = f'tag_{i}'
+        data['standalone_tags']['tag_names'].append(tag_name)
+        data['standalone_tags'][tag_name] = dict(id=i, size=size)
     return data
+
+
+def generate_tag_bundle(tag_poses, name: str):
+    data = {f'{name}': dict(ids=[])}
+    for tag in tag_poses['tag_poses']:
+        data[name]['ids'].append(tag['id'])
+        data[name][tag["id"]] = dict(
+            size=tag['size'],
+            x=tag['x'],
+            y=tag['y'],
+            z=tag['z'],
+            qw=1.0,
+            qx=0.0,
+            qy=0.0,
+            qz=0.0,
+        )
+    return data
+
+
+def convert_to_rosparam(data):
+    return {'/**': {'ros__parameters': data}}
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tag-size', default=0.04, help='Tag size in meters.')
+    parser.add_argument('--tag-size',
+                        default=0.05,
+                        help='Tag size in meters including the border.')
     parser.add_argument('--grid-size',
                         nargs=2,
-                        default=[23, 14],
+                        default=[13, 7],
                         type=int,
                         help='Number of rows and cols of the tag grid.')
     parser.add_argument('--offset',
                         nargs=3,
-                        default=[0.2, 0.2, -1.0],
+                        default=[0.2, 0.3, -1.45],
                         type=float,
                         help='Offset of the tag grid relative to the origin')
     parser.add_argument('--distance',
                         nargs=2,
-                        default=[0.2, 0.2],
+                        default=[0.25, 0.3],
                         type=float,
                         help='Distance between tags in x- and y-direction')
     parser.add_argument('--out-dir',
@@ -69,24 +95,34 @@ def main():
     args = parser.parse_args()
 
     yaml.add_representer(float, float_representer)
-    data = generate_even_grid(args.grid_size, args.offset, args.distance,
-                              args.tag_size)
+    pose_data = generate_even_grid(args.grid_size, args.offset, args.distance,
+                                   args.tag_size)
     if args.out_dir == '-':
-        yaml.dump(data, sys.stdout, default_flow_style=True, width=float('inf'))
+        yaml.dump(pose_data,
+                  sys.stdout,
+                  default_flow_style=True,
+                  width=float('inf'))
     else:
         filename = 'tag_poses.yaml'
         filepath = os.path.join(args.out_dir, filename)
         with open(filepath, 'w') as f:
-            yaml.dump(data, f)
+            yaml.dump(pose_data, f)
             print(f'Created file [{filepath}]')
 
         filename = 'tags_standalone.yaml'
         filepath = os.path.join(args.out_dir, filename)
+        n = args.grid_size[0] * args.grid_size[1]
+        # correct for difference in physical tag size and tag without border
+        standalone_data = generate_standalone_tags(n, args.tag_size * 8 / 10)
         with open(filepath, 'w') as f:
-            n = args.grid_size[0] * args.grid_size[1]
-            # correct for difference in physical tag size and tag without border
-            data = generate_tag_standalone(n, args.tag_size * 8 / 10)
-            yaml.dump(data, f)
+            yaml.dump(convert_to_rosparam(standalone_data), f)
+            print(f'Created file [{filepath}]')
+
+        filename = 'tag_bundle.yaml'
+        filepath = os.path.join(args.out_dir, filename)
+        bundle_data = generate_tag_bundle(pose_data, 'all_tags')
+        with open(filepath, 'w') as f:
+            yaml.dump(convert_to_rosparam(bundle_data), f)
             print(f'Created file [{filepath}]')
 
 
