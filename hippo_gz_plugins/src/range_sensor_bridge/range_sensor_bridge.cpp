51d1f9f98964d460e64696acd091fea70e43ae9b
Thies Lennart Alff <lennart.alff@gmail.com>
2023-03-27 11:55:56 +0000

    added range_sensor_bridge

---
+++ hippo_gz_plugins/src/range_sensor_bridge/range_sensor_bridge.cpp
@@ -0,0 +1,37 @@
+#include "range_sensor_bridge.hpp"
+
+namespace hippo_common {
+namespace range_sensor {
+RangeSensor::RangeSensor(rclcpp::NodeOptions const &_options)
+    : Node("range_sensor", _options) {
+  //      DeclareParams();
+  ranges_pub_ = create_publisher<hippo_msgs::msg::RangeMeasurementArray>(
+      "ranges", rclcpp::SensorDataQoS());
+  node_topics =
+      (rclcpp::node_interfaces::NodeTopics *)get_node_topics_interface().get();
+
+  std::string topic_name;
+  topic_name = node_topics->resolve_topic_name("ranges");
+  gz_node_->Subscribe(topic_name, &RangeSensor::OnRanges, this);
+}
+
+void RangeSensor::OnRanges(
+    const hippo_gz_msgs::msg::RangeMeasurementArray &_msg) {
+  rclcpp::Time t_now = now();
+  hippo_msgs::msg::RangeMeasurementArray ros_msg;
+  ros_msg.header.stamp = t_now;
+  for (int i = 0; i < _msg.measurements_size(); ++i) {
+    hippo_msgs::msg::RangeMeasurement ros_meas;
+    auto gz_meas = _msg.measurements(i);
+    ros_meas.id = gz_meas.id();
+    ros_meas.range = gz_meas.range();
+    ros_meas.header.stamp = t_now;
+    ros_msg.measurements.push_back(ros_meas);
+  }
+  ranges_pub_->publish(ros_msg);
+}
+}  // namespace range_sensor
+}  // namespace fav
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(fav::range_sensor::RangeSensor)
