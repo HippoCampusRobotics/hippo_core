da77e24b077c47f095a1c9b5248291dc7cd06af1
Thies Lennart Alff <lennart.alff@gmail.com>
2023-06-26 18:36:50 +0000

    added parameter handling and some code cleanup

--- hippo_control/include/hippo_control/trajectory_tracking/tracking_controller.hpp
+++ hippo_control/include/hippo_control/trajectory_tracking/tracking_controller.hpp
@@ -4,10 +4,11 @@
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <geometry_msgs/msg/vector3_stamped.hpp>
 #include <hippo_common/convert.hpp>
-#include <hippo_common/tf2_utils.hpp>
 #include <hippo_common/param_utils.hpp>
+#include <hippo_common/tf2_utils.hpp>
 #include <hippo_msgs/msg/attitude_target.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <rcl_interfaces/msg/set_parameters_result.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float64.hpp>
 
@@ -21,12 +22,16 @@ class TrackingControllerNode : public rclcpp::Node {
 
  private:
   struct Params {
+    bool updated{false};
     double position_gain{1.0};
     double velocity_gain{1.0};
   };
   void DeclareParams();
   void InitPublishers();
   void InitSubscriptions();
+  void UpdateControllerParams();
+  void PublishAttitudeTarget(const Eigen::Quaterniond &_attitude,
+                             double _thrust, const rclcpp::Time &_now);
 
   void OnRollTarget(const std_msgs::msg::Float64::SharedPtr _msg) {
     roll_target_ = _msg->data;
@@ -36,10 +41,15 @@ class TrackingControllerNode : public rclcpp::Node {
       const geometry_msgs::msg::PointStamped::SharedPtr _msg) {
     hippo_common::convert::RosToEigen(_msg->point, position_target_);
   }
+
+  rcl_interfaces::msg::SetParametersResult OnParams(
+      const std::vector<rclcpp::Parameter> &_parameters);
   void OnVelocityTarget(
       const geometry_msgs::msg::Vector3Stamped::SharedPtr _msg) {
     hippo_common::convert::RosToEigen(_msg->vector, velocity_target_);
   }
+
+  OnSetParametersCallbackHandle::SharedPtr params_cb_handle_{nullptr};
 
   rclcpp::Publisher<hippo_msgs::msg::AttitudeTarget>::SharedPtr
       attitude_target_pub_{nullptr};
--- hippo_control/src/trajectory_tracking/tracking_controller.cpp
+++ hippo_control/src/trajectory_tracking/tracking_controller.cpp
@@ -1,11 +1,12 @@
 #include "hippo_control/trajectory_tracking/tracking_controller.hpp"
+
 namespace hippo_control {
 namespace trajectory_tracking {
 
 TrackingControllerNode::TrackingControllerNode(
     rclcpp::NodeOptions const &_options)
     : Node("tracking_controller", _options) {
-  // DeclareParams();
+  DeclareParams();
   InitPublishers();
   InitSubscriptions();
 }
@@ -49,27 +50,43 @@ void TrackingControllerNode::InitSubscriptions() {
       });
 }
 
+void TrackingControllerNode::UpdateControllerParams() {
+  if (params_.updated) {
+    params_.updated = false;
+    controller_.SetPositionGain(params_.position_gain);
+    controller_.SetVelocityGain(params_.velocity_gain);
+  }
+}
+
 void TrackingControllerNode::OnOdometry(
     const nav_msgs::msg::Odometry::SharedPtr _msg) {
   hippo_common::convert::RosToEigen(_msg->pose.pose.position, position_);
   hippo_common::convert::RosToEigen(_msg->twist.twist.linear, velocity_);
   hippo_common::convert::RosToEigen(_msg->pose.pose.orientation, attitude_);
   controller_.SetDesiredState(position_target_, velocity_target_, roll_target_);
+  UpdateControllerParams();
   // TODO: add feedforward term based on some model!
   attitude_target_ =
       controller_.Update(position_, velocity_, Eigen::Vector3d::Zero());
-  hippo_msgs::msg::AttitudeTarget msg;
-  msg.header.stamp = now();
-  msg.header.frame_id = hippo_common::tf2_utils::frame_id::InertialFrame();
-  hippo_common::convert::EigenToRos(attitude_target_, msg.attitude);
   thrust_ = controller_.Thrust(attitude_);
-  msg.thrust = thrust_;
+  rclcpp::Time t_now = now();
+  PublishAttitudeTarget(attitude_target_, thrust_, t_now);
+}
+
+void TrackingControllerNode::PublishAttitudeTarget(
+    const Eigen::Quaterniond &_attitude, double _thrust,
+    const rclcpp::Time &_now) {
   if (attitude_target_pub_ == nullptr) {
     RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 1000,
                          "Publisher for attitude_target not available");
     return;
   }
-  attitude_target_pub_->publish(msg);
+  auto msg = std::make_unique<hippo_msgs::msg::AttitudeTarget>();
+  msg->header.stamp = _now;
+  msg->header.frame_id = hippo_common::tf2_utils::frame_id::InertialFrame();
+  hippo_common::convert::EigenToRos(_attitude, msg->attitude);
+  msg->thrust = _thrust;
+  attitude_target_pub_->publish(std::move(msg));
 }
 
 }  // namespace trajectory_tracking
