9b43d02670b544dc3cc95a43e8e72ab6cef0e8af
Thies Lennart Alff <lennart.alff@gmail.com>
2024-01-31 15:37:47 +0000

    launch file cleanup

--- hippo_common/launch/tf_publisher_hippo.launch.py
+++ hippo_common/launch/tf_publisher_hippo.launch.py
@@ -1,47 +1,49 @@
-import launch
-import launch_ros
 from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, PushROSNamespace
+
+from hippo_common.launch_helper import (
+    LaunchArgsDict,
+    declare_vehicle_name_and_sim_time,
+)
 
 
-def generate_launch_description():
+def declare_launch_args(launch_description: LaunchDescription):
+    declare_vehicle_name_and_sim_time(launch_description=launch_description)
+
     package_name = 'hippo_common'
     package_path = get_package_share_path(package_name)
     default_config_vehicle_file = package_path / (
         'config/transformations_hippo_default.yaml')
-
-    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time')
-    vehicle_name = launch.substitutions.LaunchConfiguration('vehicle_name')
-
-    vehicle_name_arg = launch.actions.DeclareLaunchArgument(
-        name='vehicle_name', description='Vehicle name used as namespace.')
-    use_sim_time_arg = launch.actions.DeclareLaunchArgument(
-        name='use_sim_time', description='decide if simulation time is used.')
-    tf_config_vehicle_file_arg = launch.actions.DeclareLaunchArgument(
-        name='tf_config_vehicle_file',
+    action = DeclareLaunchArgument(
+        name='tf_vehicle_config_file',
         description='TF config file.',
         default_value=str(default_config_vehicle_file))
+    launch_description.add_action(action)
 
-    launch_args = [
-        vehicle_name_arg,
-        use_sim_time_arg,
-        tf_config_vehicle_file_arg,
-    ]
 
-    nodes_group = launch.actions.GroupAction([
-        launch_ros.actions.PushRosNamespace(vehicle_name),
-        launch_ros.actions.Node(package=package_name,
-                                executable='tf_publisher_vehicle_node',
-                                output='screen',
-                                parameters=[
-                                    {
-                                        'vehicle_name': vehicle_name,
-                                        'use_sim_time': use_sim_time,
-                                    },
-                                    launch.substitutions.LaunchConfiguration(
-                                        'tf_config_vehicle_file'),
-                                ]),
+def create_tf_publisher_node():
+    args = LaunchArgsDict()
+    args.add_vehicle_name_and_sim_time()
+    return Node(
+        package='hippo_common',
+        executable='tf_publisher_vehicle_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[args, LaunchConfiguration('tf_vehicle_config_file')],
+    )
+
+
+def generate_launch_description():
+    launch_description = LaunchDescription()
+    declare_launch_args(launch_description=launch_description)
+
+    action = GroupAction([
+        PushROSNamespace(LaunchConfiguration('vehicle_name')),
+        create_tf_publisher_node(),
     ])
+    launch_description.add_action(action)
 
-    return launch.LaunchDescription(launch_args + [
-        nodes_group,
-    ])
+    return launch_description
